77c0f6849e81970009e391460e58dbaf4cc6c32c
Frank Boensch <fboensch@hightechhigh.org>
2017-01-29 16:49:37 -0800

    updated code from 2nd PID test

--- src/org/usfirst/frc/team4014/steamworks/autonomous/PidPivotByGyro.java
+++ src/org/usfirst/frc/team4014/steamworks/autonomous/PidPivotByGyro.java
@@ -2,31 +2,42 @@ package org.usfirst.frc.team4014.steamworks.autonomous;
 
 import org.usfirst.frc.team4014.steamworks.drivetrain.DriveTrain;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.command.PIDCommand;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PidPivotByGyro extends PIDCommand {
 	
-	private final Gyro gyro;
+	private final AnalogGyro gyro;
 	private final DriveTrain driveTrain;
 	
-	public PidPivotByGyro(Gyro gyro, DriveTrain driveTrain, double angle) {
+	public PidPivotByGyro(AnalogGyro gyro, DriveTrain driveTrain, double angle) {
 		super(.7, 0, 0);
 		this.gyro = gyro;
 		this.driveTrain = driveTrain;
 		setSetpoint(angle);
 		getPIDController().setAbsoluteTolerance(.75);
+		gyro.reset();
+		gyro.setSensitivity(0.007);
+		SmartDashboard.putNumber("Gyro PID Output", 0);
+		SmartDashboard.putNumber("Gyro Angle", 0);
+		SmartDashboard.putNumber("Gyro PID Position", 0);
+		SmartDashboard.putNumber("Gyro PID Setpoint", angle);
+		SmartDashboard.putBoolean("Gyro PID Is On Target", false);
 	}
 
 	@Override
 	protected double returnPIDInput() {
-		return gyro.getAngle();
+		double a = gyro.getAngle();
+		SmartDashboard.putNumber("Gyro Angle", a);
+		return a;
 	}
 
 	@Override
 	protected void usePIDOutput(double output) {
 		SmartDashboard.putNumber("Gyro PID Output", output);
+		SmartDashboard.putNumber("Gyro PID Position", getPosition());
+		SmartDashboard.putNumber("Gyro PID Setpoint", getSetpoint());
 		if(getSetpoint() > gyro.getAngle()) {
 			driveTrain.drive(output, -output);
 		} else {
@@ -36,7 +47,12 @@ public class PidPivotByGyro extends PIDCommand {
 
 	@Override
 	protected boolean isFinished() {
-		return getPIDController().onTarget();
+		boolean onTarget = getPIDController().onTarget();
+		SmartDashboard.putBoolean("Gyro PID Is On Target", onTarget);
+		if(onTarget) {
+			getPIDController().disable();
+		}
+		return onTarget;
 	}
 
 }
--- src/org/usfirst/frc/team4014/steamworks/autonomous/PivotTest.java
+++ src/org/usfirst/frc/team4014/steamworks/autonomous/PivotTest.java
@@ -4,10 +4,9 @@ import org.usfirst.frc.team4014.steamworks.drivetrain.DriveTrain;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class PivotTest extends CommandGroup{
-	private Gyro gyro;
+	private AnalogGyro gyro;
 	
 	public PivotTest(DriveTrain driveTrain) {
 		gyro = new AnalogGyro(1);
