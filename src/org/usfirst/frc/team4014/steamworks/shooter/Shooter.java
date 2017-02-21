b57b665d2e5ead5d711d456f93abd8ab9a332fbc
Evan Mark <Evan Mark>
2017-02-21 09:12:35 -0800

    Implemented all the voltage code into master manually to avoid merge conflicts.

--- src/org/usfirst/frc/team4014/steamworks/shooter/Shooter.java
+++ src/org/usfirst/frc/team4014/steamworks/shooter/Shooter.java
@@ -4,9 +4,12 @@ import org.usfirst.frc.team4014.steamworks.CAN;
 import org.usfirst.frc.team4014.steamworks.OI;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shooter extends Subsystem {
@@ -23,26 +26,29 @@ public class Shooter extends Subsystem {
 	
 	public Shooter(OI oi) {
 		this.oi = oi;
+		Button shooterButton = new JoystickButton(this.oi.getMateJoystick(), 3);
+		shooterButton.toggleWhenPressed(new ShootWithJoystick(this.oi, this));
 	}
 	
 	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		setDefaultCommand(new ShootWithJoystick(oi, this));
 	}
 	
 	public void shoot(Joystick joystick) {
-		if(joystick.getRawButton(3)){
-			shooterMotorOne.set(-1);
-		}
-		else{
-			shooterMotorOne.set(0);
-		}
+		shooterMotorOne.changeControlMode(TalonControlMode.Voltage);
+		shooterMotorTwo.changeControlMode(TalonControlMode.Voltage);
+		shooterMotorOne.setVoltageCompensationRampRate(12.0);
+		shooterMotorTwo.setVoltageCompensationRampRate(12.0);
+		shooterMotorOne.set(-(0.25 * (joystick.getZ()) + 0.75) * 12);
+		shooterMotorTwo.set((0.25 * (joystick.getZ()) + 0.75) * 12);
+
 	}
 	
 	public void stop() {
 		shooterMotorOne.set(0);
+		shooterMotorTwo.set(0);
 	}
 	
 	// TODO: set the angles once the cad/placement of the servos is done
